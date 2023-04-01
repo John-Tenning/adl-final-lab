d9a42c75d278cc0d815fcbab871c94b3cd633d2c
Sashti Amar <62827604+John-Tenning@users.noreply.github.com>
2023-04-01 08:47:36 +0530

    Add files via upload

--- gui.java
+++ gui.java
@@ -1,32 +1,60 @@
-import android.app.Activity;
-import android.graphics.Typeface;
+package com.example.exno1;
+ 
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-public class MainActivity extends Activity {
-
+ 
+public class MainActivity extends AppCompatActivity
+{
+    int ch=1;
+    float font=30;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Get references to the GUI components
-        TextView headerText = (TextView) findViewById(R.id.headerText);
-        EditText inputText = (EditText) findViewById(R.id.inputText);
-        Button submitButton = (Button) findViewById(R.id.submitButton);
-        ImageView logoImage = (ImageView) findViewById(R.id.logoImage);
-
-        // Customize the fonts and colors of the GUI components
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf");
-        headerText.setTypeface(typeface);
-        inputText.setTypeface(typeface);
-        submitButton.setTypeface(typeface);
-
-        headerText.setTextColor(getResources().getColor(R.color.colorAccent));
-        submitButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-        logoImage.setImageResource(R.drawable.logo);
+        final TextView t= (TextView) findViewById(R.id.textView);
+        Button b1= (Button) findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                t.setTextSize(font);
+                font = font + 5;
+                if (font == 50)
+                    font = 30;
+            }
+        });
+        Button b2= (Button) findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (ch) {
+                    case 1:
+                        t.setTextColor(Color.RED);
+                        break;
+                    case 2:
+                        t.setTextColor(Color.GREEN);
+                        break;
+                    case 3:
+                        t.setTextColor(Color.BLUE);
+                        break;
+                    case 4:
+                        t.setTextColor(Color.CYAN);
+                        break;
+                    case 5:
+                        t.setTextColor(Color.YELLOW);
+                        break;
+                    case 6:
+                        t.setTextColor(Color.MAGENTA);
+                        break;
+                }
+                ch++;
+                if (ch == 7)
+                    ch = 1;
+            }
+        });
     }
 }
